75b2489db4178c633e7904a6bfc2fc7172a47af9
Thynne <khunthynne@gmail.com>
2025-06-23 06:07:47 +0700

    Add prettierrc

--- frontend/xdstudio/src/app/prove/page.tsx
+++ frontend/xdstudio/src/app/prove/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
 
   const [test, setTest] = useState();
   return (
-    <div className=" min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20 grid">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       {test}
       {testx}
     </div>
