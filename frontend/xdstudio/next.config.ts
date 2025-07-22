a86f1a1f27881f6dd7e3cfcf4dbba743a9d64868
Thynne <khunthynne@gmail.com>
2025-07-23 00:13:47 +0700

    chore: update

--- frontend/xdstudio/next.config.ts
+++ frontend/xdstudio/next.config.ts
@@ -1,7 +1,21 @@
+import { env } from "@/env";
 import "@configs/dotenv.config";
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: env.API_BACKEND_URL,
+      pathname: "/images/**",
+    },
+    {
+      protocol: "http",
+      hostname: env.API_BACKEND_URL,
+      pathname: "/images/**",
+    },
+  ],
+};
 const withNextIntl = createNextIntlPlugin("./src/libs/i18n/request.ts");
 export default withNextIntl(nextConfig);
